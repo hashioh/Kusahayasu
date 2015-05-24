4298f032543a35dc6210064491557053ffede30a
kazuki ohashi <kensapo.zbn@gmail.com>
2015-05-24 23:36:48 +0900

    Initial Commit

---
+++ Kusahayasu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kusahayasu
+//
+//  Created by Kazuki Ohashi on 2015/05/24.
+//  Copyright (c) 2015年 Kazuki Ohashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
