5a9db4ff1a1cb1b0e196b849e6f85cf361ef48bd
kazuki ohashi <kensapo.zbn@gmail.com>
2015-05-25 14:58:11 +0900

    remove code

--- Kusahayasu/ViewController.swift
+++ Kusahayasu/ViewController.swift
@@ -30,37 +30,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate , UINavi
         //処理を書く
         if imageIndex != 0 {
             var image: UIImage = UIImage(named: imageNameArray[imageIndex - 1])!
-            imageView = UIImageView(frame: CGRectMake(0, 0, 40, 40))
+            imageView = UIImageView(frame: CGRectMake(0, 0, 20, 20))
             imageView.image = image
             imageView.center = CGPointMake(location.x, location.y)
             self.view.addSubview(imageView)
         }
     }
     
-    @IBAction func back() {
-        self.imageView.removeFromSuperview()
-    }
-    
-    @IBAction func selectBackground() {
-        var imagePickerController: UIImagePickerController = UIImagePickerController()
-        imagePickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
-        imagePickerController.delegate = self
-        imagePickerController.allowsEditing = true
-        self.presentViewController(imagePickerController, animated: true, completion: nil)
-        
-    }
-    
-    @IBAction func save() {
-        
-        let rect: CGRect = CGRectMake(0, 30, 320, 380)
-        UIGraphicsBeginImageContext(rect.size)
-        self.view.layer.renderInContext(UIGraphicsGetCurrentContext())
-        let capture = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        UIImageWriteToSavedPhotosAlbum(capture, nil, nil, nil)
-        UIGraphicsEndImageContext()
-    }
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
         var image: UIImage = info[UIImagePickerControllerEditedImage] as! UIImage
